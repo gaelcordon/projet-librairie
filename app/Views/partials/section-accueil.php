ca4f1941d413b00c628e202db657cc223b6bed55
gaelcordon <cordon.gael@gmail.com>
2017-03-28 16:38:48 +0200

    Carousel et page la librairie

--- app/Views/partials/section-accueil.php
+++ app/Views/partials/section-accueil.php
@@ -1,48 +1,142 @@
-			<section id="sectionCentraleAccueil" class="col-xs-12 col-sm-11">
-				<div class="parallax">
-					<article id="texteAccueil" class="text-center parallaxRideau">
+			<section  class="parallax col-xs-12 col-sm-11">
+					<div id="carouselNouveautes" class="container parallaxRideau">
+						<div class="row threed-section">
+						  <div class="col-md-12">
+						    <span href="#" class="span-featured2">Hello World</span>
+
+						    <div class="carousel carousel-multiItem slide" id="carousel-item">
+						      <div class="carousel-inner">
+						        <div class="item active">
+						          <div class="col-xs-12 col-sm-4 col-md-4 hover">
+						            <a href="#" ><img src="http://demo.wpzoom.com/domino/files/2013/03/photodune-2064182-moscow-m-1000x500.jpg" class="img-responsive"></a>
+						            <div class="data-enter" id="data-enter">
+
+						              <h3><a>Benfica crowned 2014/15 Champions of Portugal</a></h3>
+
+						              <span class="date">May 29,2015</span>
+						              <span class="comment">0 Comment</span>
+						            </div>
+						          </div>
+						        </div>
+						        <div class="item">
+						          <div class="col-xs-12 col-sm-4 col-md-4 hover">
+						            <a href="#" ><img src="http://demo.wpzoom.com/domino/files/2010/02/bank-station-entrance-in-london-1000x500.jpg" class="img-responsive"></a>
+						            <div class="data-enter" id="data-enter">
+
+						              <h3><a>Putin election victory: What next for Russia?</a></h3>
+
+						              <span class="date">May 29,2015</span>
+						              <span class="comment">0 Comment</span>
+						            </div>
+						          </div>
+						        </div>
+						        <div class="item">
+						          <div class="col-xs-12 col-sm-4 col-md-4 hover">
+						            <a href="#" ><img src="http://demo.wpzoom.com/domino/files/2013/04/tumblr_mzx9rx6HMZ1slhhf0o1_1280-1000x500.jpg" class="img-responsive"></a>
+
+						            <div class="data-enter" id="data-enter">
+
+						              <h3><a>Putin election victory: What next for Russia?</a></h3>
+
+						              <span class="date">May 29,2015</span>
+						              <span class="comment">0 Comment</span>
+						            </div>
+						          </div>
+						        </div>
+						        <div class="item">
+						          <div class="col-xs-12 col-sm-4 col-md-4 hover">
+						            <a href="#" ><img src=" http://demo.wpzoom.com/domino/files/2010/02/bank-station-entrance-in-london-1000x500.jpg" class="img-responsive"></a>
+						            <div class="data-enter" id="data-enter">
+
+						              <h3><a>Putin election victory: What next for Russia?</a></h3>
+
+						              <span class="date">May 29,2015</span>
+						              <span class="comment">0 Comment</span>
+						            </div>
+
+						          </div>
+						        </div>
+						        <div class="item">
+						          <div class="col-xs-12 col-sm-4 col-md-4 hover ">
+						            <a href="#" ><img src="http://demo.wpzoom.com/domino/files/2015/02/tumblr_nokjp3Im5K1tomxvuo3_1280-1024x660.jpg" class="img-responsive"></a>
+
+						            <div class="data-enter" id="data-enter">
+
+						              <h3><a>Benfica crowned 2014/15 Champions of Portugal</a></h3>
+
+						              <span class="date">May 29,2015</span>
+						              <span class="comment">0 Comment</span>
+						            </div>
+						          </div>
+						        </div>
+						        <div class="item">
+						          <div class="col-xs-12 col-sm-4 col-md-4  hover ">
+						            <a href="#" ><img src="http://demo.wpzoom.com/domino/files/2010/02/bank-station-entrance-in-london-1000x500.jpg" class="img-responsive"></a>
+						            <div class="data-enter" id="data-enter">
+
+						              <h3><a>Benfica crowned 2014/15 Champions of Portugal</a></h3>
+
+						              <span class="date">May 29,2015</span>
+						              <span class="comment">0 Comment</span>
+						            </div>
+						          </div>
+						        </div>
+						        <div class="item">
+						          <div class="col-xs-12  col-sm-4 col-md-4 hover ">
+						            <a href="#" ><img src="http://demo.wpzoom.com/domino/files/2010/02/bank-station-entrance-in-london-1000x500.jpg" class="img-responsive"></a>
+
+						            <div class="data-enter" id="data-enter">
+
+						              <h3><a>Benfica crowned 2014/15 Champions of Portugal</a></h3>
+
+						              <span class="date">May 29,2015</span>
+						              <span class="comment">0 Comment</span>
+						            </div>
+						          </div>
+						        </div>
+						        <div class="item">
+						          <div class="col-xs-12 col-sm-4 col-md-4  hover ">
+						            <a href="#" ><img src="http://demo.wpzoom.com/domino/files/2013/03/photodune-2064182-moscow-m-1000x500.jpg" class="img-responsive"></a>
+						            <div class="data-enter" id="data-enter">
+
+						              <h3><a>Benfica crowned 2014/15 Champions of Portugal</a></h3>
+
+						              <span class="date">May 29,2015</span>
+						              <span class="comment">0 Comment</span>
+						            </div>
+						          </div>
+						        </div>
+						      </div>
+						      <a class="left carousel-control" href="#carousel-item" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+						      <a class="right carousel-control" href="#carousel-item" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+						    </div>
+						  </div>
+						</div>
+					</div>
+				</div>
+
+				<article id="texteAccueil" class="text-center parallaxRideau">
+					<section id="ilEtaitUneFois">
 						<h2>Il était une fois...</h2>
-						<p>La Librairie A la gloire de mon père a ouvert ses portes le 18 Avril 2009.<br />
-						Dans une surface d’environ 120 m2, nous vous proposons :</p>
-						<section id="espaceAdulte">
-							<h2>Un espace dédié à la littérature adulte</h2>
-							<ul>
-								<li>Littérature française</li>
-								<li>Littérature étrangère</li>
-								<li>Littérature policière</li>
-								<li>Bande Dessinée Adulte et Jeunesse</li>
-								<li>Développement personnel, Santé, Psychologie</li>
-								<li>Cuisine, Vie Pratique</li>
-							</ul>
-						</section>
-						<section id="espaceJeunesse">
-							<h2>Un espace dédié à la Jeunesse</h2>
-							<ul>
-								<li>Documentaires</li>
-								<li>Premières lectures</li>
-								<li>Romans Jeunesse et pré-Ados</li>
-								<li>Romans Ados et Young Adult</li>
-								<li>Mangas</li>
-								<li>Albums</li>
-								<li>Jeux créatifs et de société</li>
-							</ul>
-						</section>
-						<section id="espacePapeterie">
-							<h2>Un espace dédié à la Papeterie et aux Loisirs créatifs</h2>
-							<ul>
-								<li>Papeterie scolaire ou de bureau</li>
-								<li>Décopatch</li>
-								<li>Jouets d’éveil</li>
-								<li>Matériel de loisir créatif</li>
-							</ul>
-						</section>
-						<section id="vousAccompagner">
-							<h2>Pour vous accompagner</h2>
-							<p>Deux Libraires sont présentes pour vous conseiller et vous accompagner dans vos recherches. Vous pourrez trouver ici et là nos coups de cœur, n’hésitez pas à nous solliciter…<br />
-							Commerce de proximité, nous privilégions les relations humaines, aimons échanger sur nos et vos dernières lectures… Partage, échange, convivialité sont les maîtres mots.<br />
-							Parfois nous nous plaisons à recevoir un auteur pour une rencontre et dédicace.<br />
-							Vous pouvez également nous rencontrer au cours de Salons, festivals.</p>
-						</section>
-					</article>
-				</div><!--class="parallaxAccueil"-->
-			</section> <!--sectionCentraleAccueil
+						<p>
+						Les portes de la librairie, "A la gloire de mon père", se sont ouvertes, <br />
+						la première fois, le 18 avril 2009 grâce à la détermination de sa propriétaire Eve Arnoux. <br />
+						En dépit d’un contexte peu favorable face à une concurrence non spécialisée présente sur Internet, <br />
+						la preuve est faite qu’une librairie généraliste indépendante a su trouver sa place au sein <br />
+						d’un petit village du Vaucluse. Située non loin de la place de la mairie de Mazan, ce sont près <br />
+						de 3500 titres en stock que nous vous proposons sur une surface d’environ 120 m<sup>2</sup>.
+						</p>
+					</section>
+																
+					<section id="vousAccompagner">
+						<h2>Pour vous accompagner</h2>
+						<p>
+						Deux Libraires sont présentes pour vous conseiller et vous accompagner dans vos recherches. <br />
+						Vous pourrez trouver ici et là nos coups de cœur, n’hésitez pas à nous solliciter…<br />
+						Commerce de proximité, nous privilégions les relations humaines, aimons échanger sur nos et vos dernières lectures…<br />
+						Partage, échange, convivialité sont les maîtres mots.<br />
+						Parfois nous nous plaisons à recevoir un auteur pour une rencontre et dédicace.<br />
+						Vous pouvez également nous rencontrer au cours de Salons, festivals.</p>
+					</section>
+				</article><!--id="texteAccueil"-->
+			</section> <!--class="parallax" -->
