0e3f621b7876d38d1649d7d0108a61a141430434
Gael CORDON <cordon.gael@gmail.com>
2017-04-04 15:51:09 +0200

    commit

--- app/Views/partials/section-accueil.php
+++ app/Views/partials/section-accueil.php
@@ -1,7 +1,7 @@
 		<section id="bgAccueil" class="parallax">
 			<div id="nouveautes" class="container">
-				<div class="row paddingTop">
-					<div class="blocText parallaxRideau col-centered text-center col-md-8">
+				<div class="row parallaxRideau">
+					<div class="blocText text-center">
 						<h2>Nouveautés</h2>
 					</div>
 				</div>
@@ -42,9 +42,9 @@
 				</div> -->
 			</div><!--id="nouveautes"-->
 			<div class="container">
-				<div class="row paddingTop">
+				<div class="row">
 					<div class="col-xs-10 col-sm-8 col-md-8 col-centered">
-						<div id="texteAccueil" class="text-center blocText parallaxRideau">
+						<div class="text-center blocText parallaxRideau">
 							<div id="ilEtaitUneFois">
 								<h2>Il était une fois...</h2>
 								<p>
@@ -76,19 +76,19 @@
 
 		<section id="bgBottomAccueil" class="parallax">
 			<div class="container-fluid">
-				<div id="adresseSection" class="row paddingTop blocText">
+				<div id="adresseSection" class="row blocText">
 					<div id="divIframe" class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-2">
 						<iframe id="localisation" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2867.2553174584305!2d5.125018715696843!3d44.05743623421457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ca76a77dea7fc5%3A0xefb2cbcce49b402f!2sLa+Gloire+De+Mon+P%C3%A8re+Mazan!5e0!3m2!1sfr!2sfr!4v1485903280277" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
 					</div>
-					<div class="row col-md-6">
-						<div class="col-md-4 col-xs-5 col-xs-offset-1 text-center">
+					<div class="col-md-6">
+						<div class="col-md-4 col-xs-5 col-xs-offset-2 text-center">
 							<div id="adresse">
 								<h2>Nous Trouver</h2>
 								<address>
 									<strong>La Gloire De Mon Père</strong><br>
 									265 Avenue de l'Europe<br />
 									84380 Mazan<br />
-									<abbr title="Phone"> 04 90 30 94 99 </abbr>
+									<span>04 90 30 94 99</span>
 								</address>
 							</div>
 						</div>
@@ -97,8 +97,8 @@
 								<h2>Horaires d'ouverture</h2>
 								<p>du Mardi au <br />
 								Samedi<br />
-								de 9h à 12h30<br />
-								et de 14h à 19h<br /></p>
+								de <span>9h</span> à <span>12h30</span><br />
+								et de <span>14h</span> à <span>19h</span><br /></p>
 							</div>
 						</div>
 						<div id="sectionFormContact" class="col-xs-8 col-xs-offset-2 col-md-7">
--- app/Views/partials/section-la-librairie.php
+++ app/Views/partials/section-la-librairie.php
@@ -1,7 +1,7 @@
 <section id="bgLaLibrairie" class="parallax">
 	<div class="container">
-		<div class="row paddingTop">
-			<div class="blocText parallaxRideau col-centered text-center col-md-10">
+		<div class="row parallaxRideau">
+			<div class="blocText col-centered text-center col-md-10">
 
 				<h2>Trois espaces sont à votre disposition.</h2>
 				<p>Sur une surface d’environ <span>120 m<sup>2</sup></span>,
@@ -9,57 +9,105 @@
 
 			</div>
 		</div>
+		<div id="espaceAdulte" class="row parallaxRideau">
+			<div class="blocText text-center col-md-2 col-same-height">
+				<ul>
+					<li><h2>Littérature adulte</h2></li>
+					<li>Littérature française</li>
+					<li>Littérature étrangère</li>
+					<li>Littérature policière</li>
+					<li>Bande Dessinée Adulte et Jeunesse</li>
+					<li>Développement personnel, Santé, Psychologie</li>
+					<li>Cuisine, Vie Pratique</li>
+				</ul>
+			</div>
+			<div class="selectionLivres col-md-9 col-md-offset-1 col-same-height">
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+				</div>
+				<div class="espaceVide row">
+				</div>
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="espaceJeunesse" class="row parallaxRideau">
+			<div class="blocText text-center col-md-2 col-same-height">
+				<ul>
+					<li><h2>Jeunesse</h2></li>
+					<li>Documentaires</li>
+					<li>Premières lectures</li>
+					<li>Romans Jeunesse et pré-Ados</li>
+					<li>Romans Ados et Young Adult</li>
+					<li>Mangas</li>
+					<li>Albums</li>
+					<li>Jeux créatifs et de société</li>
+				</ul>
+			</div>
+			<div class="selectionLivres col-md-9 col-md-offset-1 col-same-height">
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+				</div>
+				<div class="espaceVide row">
+				</div>
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="espacePapeterie" class="row parallaxRideau">	
+			<div class="blocText text-center col-md-2 col-same-height">
+				<ul>
+					<li><h2>Papeterie <br />
+					Loisirs créatifs</h2></li>
+					<li>Papeterie scolaire ou de bureau</li>
+					<li>Décopatch</li>
+					<li>Jouets d’éveil</li>
+					<li>Matériel de loisir créatif</li>
+				</ul>
+			</div>
+			<div class="selectionLivres col-md-9 col-md-offset-1 col-same-height">
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+				</div>
+				<div class="espaceVide row">
+				</div>
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+				</div>
+			</div>
 
-		<div id="espaceAdulte" class="row paddingTop">
-			<div class="container col-md-10 col-centered blocText parallaxRideau">
-				<div class="menuGenre text-center col-md-4 col-same-height">
-					<ul>
-						<li><h2>Littérature adulte</h2></li>
-						<li>Littérature française</li>
-						<li>Littérature étrangère</li>
-						<li>Littérature policière</li>
-						<li>Bande Dessinée Adulte et Jeunesse</li>
-						<li>Développement personnel, Santé, Psychologie</li>
-						<li>Cuisine, Vie Pratique</li>
-					</ul>
-				</div>
-				<div class="presentationLivres col-md-6 col-md-offset-2 col-same-height">		
-				</div>
-			</div>
-		</div>
-		<div id="espaceJeunesse"class="row paddingTop">
-			<div class="container col-md-10 col-centered blocText parallaxRideau">
-				<div class="menuGenre text-center col-md-4 col-same-height">
-					<ul>
-						<li><h2>Jeunesse</h2></li>
-						<li>Documentaires</li>
-						<li>Premières lectures</li>
-						<li>Romans Jeunesse et pré-Ados</li>
-						<li>Romans Ados et Young Adult</li>
-						<li>Mangas</li>
-						<li>Albums</li>
-						<li>Jeux créatifs et de société</li>
-					</ul>
-				</div>
-			</div>
-			<div class="presentationLivres col-md-6 col-md-offset-2 col-same-height">			
-			</div>
-		</div>
-		<div id="espacePapeterie" class="row paddingTop">	
-			<div class="container col-md-10 col-centered blocText parallaxRideau">
-				<div class="menuGenre text-center col-md-4 col-same-height">
-					<ul>
-						<li><h2>Papeterie <br />
-						Loisirs créatifs</h2></li>
-						<li>Papeterie scolaire ou de bureau</li>
-						<li>Décopatch</li>
-						<li>Jouets d’éveil</li>
-						<li>Matériel de loisir créatif</li>
-					</ul>
-				</div>
-			</div>
-			<div class="presentationLivres col-md-6 col-md-offset-2 col-same-height">			
-			</div>
 		</div>
 	</div>
 </section> <!-- id="bgLaLibrairie" class="parallax" -->
