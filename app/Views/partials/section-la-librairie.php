13560d91388a37ac40eca45bcf6be70b612a5d09
Skred04 <pier.philippon@gmail.com>
2017-04-04 15:54:13 +0200

    Commit

--- app/Views/partials/section-la-librairie.php
+++ app/Views/partials/section-la-librairie.php
@@ -1,3 +1,38 @@
+<?php
+	function createDivLibrairie ($sousGenre)
+	{
+		$objetLivresModel = new \Model\LivresModel;
+
+		$tabToken = ["id_sousgenre" => $sousGenre];
+
+		$tabLigne = $objetLivresModel->search($tabToken, $operator = "AND");
+
+		$i = 0;
+
+		foreach ($tabLigne as $LigneCourante) {
+			$i++;
+			if ($i <7)
+			{
+				$img = $LigneCourante['couverture'];
+				$titre = $LigneCourante['titreLivre'];
+
+				echo
+<<<CODEHTML
+				<div>
+					<p>$img</p>
+					<p>$titre</p>
+				</div>
+CODEHTML;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+?>
+
+
 <section id="bgLaLibrairie" class="parallax">
 	<div class="container">
 		<div class="row paddingTop">
@@ -15,15 +50,16 @@
 				<div class="menuGenre text-center col-md-4 col-same-height">
 					<ul>
 						<li><h2>Littérature adulte</h2></li>
-						<li>Littérature française</li>
-						<li>Littérature étrangère</li>
-						<li>Littérature policière</li>
-						<li>Bande Dessinée Adulte et Jeunesse</li>
-						<li>Développement personnel, Santé, Psychologie</li>
-						<li>Cuisine, Vie Pratique</li>
+						<li id="française">Littérature française</li>
+						<li id="étrangère">Littérature étrangère</li>
+						<li id="thriller">Littérature policière</li>
+						<li id="albums">Bande Dessinée Adulte et Jeunesse</li>
+						<li id="developpement">Développement personnel, Santé, Psychologie</li>
+						<li id="cuisine">Cuisine, Vie Pratique</li>
 					</ul>
 				</div>
-				<div class="presentationLivres col-md-6 col-md-offset-2 col-same-height">		
+				<div id="divFrançaise" class="presentationLivres col-md-6 col-md-offset-2 col-same-height">
+				<?php createDivLibrairie(1) ?>	
 				</div>
 			</div>
 		</div>
@@ -32,13 +68,13 @@
 				<div class="menuGenre text-center col-md-4 col-same-height">
 					<ul>
 						<li><h2>Jeunesse</h2></li>
-						<li>Documentaires</li>
-						<li>Premières lectures</li>
-						<li>Romans Jeunesse et pré-Ados</li>
-						<li>Romans Ados et Young Adult</li>
-						<li>Mangas</li>
-						<li>Albums</li>
-						<li>Jeux créatifs et de société</li>
+						<li id="documentaire">Documentaires</li>
+						<li id="premiere">Premières lectures</li>
+						<li id="preados">Romans Jeunesse et pré-Ados</li>
+						<li id="young">Romans Ados et Young Adult</li>
+						<li id="manga">Mangas</li>
+						<li id="panini">Albums</li>
+						<li id="créatifs">Jeux créatifs et de société</li>
 					</ul>
 				</div>
 			</div>
@@ -51,10 +87,10 @@
 					<ul>
 						<li><h2>Papeterie <br />
 						Loisirs créatifs</h2></li>
-						<li>Papeterie scolaire ou de bureau</li>
-						<li>Décopatch</li>
-						<li>Jouets d’éveil</li>
-						<li>Matériel de loisir créatif</li>
+						<li id="papetrie">Papeterie scolaire ou de bureau</li>
+						<li id="decopatch">Décopatch</li>
+						<li id="jouets">Jouets d’éveil</li>
+						<li id="loisirs">Matériel de loisir créatif</li>
 					</ul>
 				</div>
 			</div>
