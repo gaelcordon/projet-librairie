98f133cd6002a39a4c095bdd348ea593b1046057
Skred04 <pier.philippon@gmail.com>
2017-04-04 16:14:31 +0200

    Commit

--- app/Views/partials/section-la-librairie.php
+++ app/Views/partials/section-la-librairie.php
@@ -13,16 +13,21 @@
 			$i++;
 			if ($i <7)
 			{
-				$img = $LigneCourante['couverture'];
+				$couverture = "img/livres/";
+				$couverture .= $LigneCourante["couverture"];
+
+				$alt = "livre_";
+				$alt .= $LigneCourante["titreLivre"];
+
 				$titre = $LigneCourante['titreLivre'];
 
-				echo
-<<<CODEHTML
-				<div>
-					<p>$img</p>
-					<p>$titre</p>
+				$objetView = new \W\View\Plates\PlatesExtensions;
+?>
+				<div class=focusLivre col-md-4>
+					<img src="<?php	echo $objetView->assetUrl($couverture); ?>" alt="<?php echo $alt; ?>" />
+					<p><?php echo $titre; ?></p>
 				</div>
-CODEHTML;
+<?php
 			}
 			else
 			{
@@ -32,11 +37,10 @@ CODEHTML;
 	}
 ?>
 
-
 <section id="bgLaLibrairie" class="parallax">
 	<div class="container">
-		<div class="row paddingTop">
-			<div class="blocText parallaxRideau col-centered text-center col-md-10">
+		<div class="row parallaxRideau">
+			<div class="blocText col-centered text-center col-md-10">
 
 				<h2>Trois espaces sont à votre disposition.</h2>
 				<p>Sur une surface d’environ <span>120 m<sup>2</sup></span>,
@@ -44,11 +48,9 @@ CODEHTML;
 
 			</div>
 		</div>
-
-		<div id="espaceAdulte" class="row paddingTop">
-			<div class="container col-md-10 col-centered blocText parallaxRideau">
-				<div class="menuGenre text-center col-md-4 col-same-height">
-					<ul>
+		<div id="espaceAdulte" class="row parallaxRideau">
+			<div class="blocText text-center col-md-2 col-same-height">
+				<ul>
 						<li><h2>Littérature adulte</h2></li>
 						<li id="française">Littérature française</li>
 						<li id="étrangère">Littérature étrangère</li>
@@ -56,46 +58,95 @@ CODEHTML;
 						<li id="albums">Bande Dessinée Adulte et Jeunesse</li>
 						<li id="developpement">Développement personnel, Santé, Psychologie</li>
 						<li id="cuisine">Cuisine, Vie Pratique</li>
-					</ul>
+				</ul>
+			</div>
+			<div class="selectionLivres col-md-9 col-md-offset-1 col-same-height">
+				<div id="divFrançaise" class="row divLibrairie">
+					<?php createDivLibrairie(2) ?>
 				</div>
-				<div id="divFrançaise" class="presentationLivres col-md-6 col-md-offset-2 col-same-height">
-				<?php createDivLibrairie(1) ?>	
+				<div id="divEtrangere" class="row divLibrairie">
+					<?php createDivLibrairie(1) ?>
+				</div>
+				<div id="divPoliciere" class="row divLibrairie">
+					<?php createDivLibrairie(3) ?>
+				</div>
+				<div id="divBDAdulte" class="row divLibrairie">
+					<?php createDivLibrairie(6) ?>
+				</div>
+				<div id="divSante" class="row divLibrairie">
+					<?php createDivLibrairie(17) ?>
+				</div>
+				<div id="divCuisine" class="row divLibrairie">
+					<?php createDivLibrairie(13) ?>
 				</div>
 			</div>
 		</div>
-		<div id="espaceJeunesse"class="row paddingTop">
-			<div class="container col-md-10 col-centered blocText parallaxRideau">
-				<div class="menuGenre text-center col-md-4 col-same-height">
-					<ul>
-						<li><h2>Jeunesse</h2></li>
-						<li id="documentaire">Documentaires</li>
-						<li id="premiere">Premières lectures</li>
-						<li id="preados">Romans Jeunesse et pré-Ados</li>
-						<li id="young">Romans Ados et Young Adult</li>
-						<li id="manga">Mangas</li>
-						<li id="panini">Albums</li>
-						<li id="créatifs">Jeux créatifs et de société</li>
-					</ul>
-				</div>
+		<div id="espaceJeunesse" class="row parallaxRideau">
+			<div class="blocText text-center col-md-2 col-same-height">
+				<ul>
+					<li><h2>Jeunesse</h2></li>
+					<li id="documentaire">Documentaires</li>
+					<li id="premiere">Premières lectures</li>
+					<li id="preados">Romans Jeunesse et pré-Ados</li>
+					<li id="young">Romans Ados et Young Adult</li>
+					<li id="manga">Mangas</li>
+					<li id="panini">Albums</li>
+					<li id="créatifs">Jeux créatifs et de société</li>
+				</ul>
 			</div>
-			<div class="presentationLivres col-md-6 col-md-offset-2 col-same-height">			
+			<div class="selectionLivres col-md-9 col-md-offset-1 col-same-height">
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+				</div>
+				<div class="espaceVide row">
+				</div>
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+				</div>
 			</div>
 		</div>
-		<div id="espacePapeterie" class="row paddingTop">	
-			<div class="container col-md-10 col-centered blocText parallaxRideau">
-				<div class="menuGenre text-center col-md-4 col-same-height">
-					<ul>
-						<li><h2>Papeterie <br />
-						Loisirs créatifs</h2></li>
-						<li id="papetrie">Papeterie scolaire ou de bureau</li>
-						<li id="decopatch">Décopatch</li>
-						<li id="jouets">Jouets d’éveil</li>
-						<li id="loisirs">Matériel de loisir créatif</li>
-					</ul>
+		<div id="espacePapeterie" class="row parallaxRideau">	
+			<div class="blocText text-center col-md-2 col-same-height">
+				<ul>
+					<li><h2>Papeterie <br />
+					Loisirs créatifs</h2></li>
+					<li id="papetrie">Papeterie scolaire ou de bureau</li>
+					<li id="decopatch">Décopatch</li>
+					<li id="jouets">Jouets d’éveil</li>
+					<li id="loisirs">Matériel de loisir créatif</li>
+				</ul>
+			</div>
+			<div class="selectionLivres col-md-9 col-md-offset-1 col-same-height">
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+				</div>
+				<div class="espaceVide row">
+				</div>
+				<div class="row">
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
+					<div class="focusLivre col-md-4">
+					</div>
 				</div>
 			</div>
-			<div class="presentationLivres col-md-6 col-md-offset-2 col-same-height">			
-			</div>
+
 		</div>
 	</div>
 </section> <!-- id="bgLaLibrairie" class="parallax" -->
