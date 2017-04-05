d7e613dc76682f2ce3961de35c8057a354a7fddb
Gael CORDON <cordon.gael@gmail.com>
2017-04-05 17:11:25 +0200

    commit

---
+++ app/Views/partials/section-perdu.php
@@ -0,0 +1,11 @@
+<section id="bgAdmin" class="parallax">
+	<div class="container"> 
+		<div class="row parallaxRideau">
+			<div class="blocText col-centered text-center col-md-8">
+				<h3>Vous êtes perdu ?</h3>
+				
+			</div>
+		</div>
+	</div>
+</section><!-- section container-->
+
