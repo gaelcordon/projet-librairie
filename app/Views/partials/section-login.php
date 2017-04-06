ed469f325f038cc22f3d6fd0ecf7351c54d32099
Skred04 <pier.philippon@gmail.com>
2017-04-06 11:58:19 +0200

    Commit
    CSS Login et modal

--- app/Views/partials/section-login.php
+++ app/Views/partials/section-login.php
@@ -1,11 +1,15 @@
 <section id="bgAdmin" class="parallax">
 	<div class="container"> 
-		<div class="row parallaxRideau">
+		<div class="col-md-8 col-centered row parallaxRideauLogin">
 			<div class="blocText col-centered text-center col-md-8">
 				<h3>Bienvenue sur la page de connexion</h3>
-				<form class="" method="POST" action="">
-					<input type="text" name="identifiant" required placeholder="VOTRE EMAIL OU USERNAME">
-					<input type="password" name="cache" required placeholder="VOTRE MOT DE PASSE">
+				<form method="POST" action="">
+					<div class="form-group">
+						<input type="text" class="form-control" name="identifiant" required placeholder="VOTRE EMAIL OU USERNAME">						
+					</div>
+					<div class="form-group">						
+						<input type="password" class="form-control" name="cache" required placeholder="VOTRE MOT DE PASSE">
+					</div>
 					<button type="submit">ME CONNECTER</button>
 					<input type="hidden" name="idForm" value="login">
 					<div class="retour">
